eb681d197589d1a8c5fc9da9850358c3f1a805fe
Navya <navyasreemorusu46@gmail.com>
2026-08-30 18:03:21 +0530

    Add spring boot project

---
+++ src/main/java/com/example/internshiptracker/InternshiptrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.internshiptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshiptrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshiptrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/internshiptracker/InternshiptrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internshiptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshiptrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
